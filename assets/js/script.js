b933445a57d0db180af2185983e2dd7fe052fb05
Justin Oeters <justin.oeters@gmail.com>
2021-09-07 19:44:45 -0400

    added marvel url with api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,5 @@
 var giphyAPIKey = "GJ5AN5QXTS58BIGwWoTemvB0NsGw84so" 
 //Marvel Public API Key
 var marvelAPIKey = "be12b101d83bcf19e343a378efbeaef7"
+
+//request marvel api url 'https://gateway.marvel.com:443/v1/public/characters?apikey=be12b101d83bcf19e343a378efbeaef7'
