9860470079c1a6cac88e53d5360f719b7b0f160d
haleywitherell <hwitherell22@gmail.com>
2021-09-07 19:41:32 -0400

    giphy url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 // Giphy API Key Variable 
 var giphyAPIKey = "GJ5AN5QXTS58BIGwWoTemvB0NsGw84so" 
+api.giphy.com/v1/gifs/random
+
 //Marvel Public API Key
 var marvelAPIKey = "be12b101d83bcf19e343a378efbeaef7"
