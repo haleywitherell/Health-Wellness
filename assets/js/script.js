d42566e02244f99336b3d12aa20ae060f0404867
haleywitherell <hwitherell22@gmail.com>
2021-09-09 19:56:46 -0400

    days of the week

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
 
+// Days of the Week Variable 
+var daysOfTheWeek = [
+    {day: 1},
+    {day: 2},
+    {day: 3},
+    {day: 3},
+    {day: 4},
+    {day: 5},
+    {day: 6},
+    {day: 7},
+];   
+
+// create days of the week sections 
+for(i=0; i<daysOfTheWeek.length; i++) {
+    var day = $("<div").text(daysOfTheWeek[i].day)
+    var dayTitle = $("h1")
+
+    day.append(dayTitle)
+}
