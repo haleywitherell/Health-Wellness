1a44066e044f794ba130a4f357d4b899e7be6735
Shawn Miller <shawnsmm@gmail.com>
2021-09-09 19:42:24 -0400

    first API search info in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
 
+
+
+
+function foodSearch(food){
+    
+    // Spoonacular API Key
+    var spoonAPIKey = "c23f00ad85984a518a8ef39763c81e2b"
+    food = "muffins";
+    var queryURL = "https://api.spoonacular.com/food/products/search?query=" + food + "&apiKey=" + spoonAPIKey;
+    
+    fetch(queryURL)
+    .then(function (response){
+        console.log(response)
+        response.json().then(function (data){
+            console.log(data);
+        });
+    });
+    
+};
+
+// Search button handler
+$("#search-btn").on("click", foodSearch);
