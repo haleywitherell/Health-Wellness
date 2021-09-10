9e051ab53020ec511adb5f2d50e561b7475a7f53
Justin Oeters <justin.oeters@gmail.com>
2021-09-09 20:52:08 -0400

    Added Exercise API

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,26 @@ function foodSearch(food){
 
 // Search button handler
 $("#search-btn").on("click", foodSearch);
+
+function exerciseSearch(){
+
+fetch("https://exercisedb.p.rapidapi.com/exercises", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "exercisedb.p.rapidapi.com",
+		"x-rapidapi-key": "947315c5d7msh8e150cb96f6debap19e15cjsn514f7b11e3e3"
+	}
+})
+.then(response => {
+	console.log(response);
+    response.json().then(function(data){
+        console.log(data);
+    })
+})
+.catch(err => {
+	console.error(err);
+});
+};
+
+exerciseSearch();
+
