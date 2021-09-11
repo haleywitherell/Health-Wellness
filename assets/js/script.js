6a0d5cc604b71e73c0a959e56d14f2107b20d1f9
Justin Oeters <justin.oeters@gmail.com>
2021-09-11 10:12:03 -0400

    locating exercise names

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,83 @@
-function foodSearch(food){
-    
-    // Spoonacular API Key
-    var spoonAPIKey = "c23f00ad85984a518a8ef39763c81e2b"
-    food = "muffins";
-    var queryURL = "https://api.spoonacular.com/food/products/search?query=" + food + "&apiKey=" + spoonAPIKey;
-    
-    fetch(queryURL)
-    .then(function (response){
-        console.log(response)
-        response.json().then(function (data){
-            console.log(data);
-        });
+// for global variables
+
+function foodSearchResults(food) {
+  // Spoonacular API Key
+  var spoonAPIKey = "c23f00ad85984a518a8ef39763c81e2b";
+  // food = "muffins"; for test purposes
+  var queryURL =
+    "https://api.spoonacular.com/food/products/search?query=" +
+    food +
+    "&apiKey=" +
+    spoonAPIKey;
+
+  fetch(queryURL).then(function (foodResponse) {
+    console.log(foodResponse);
+    response.json().then(function (foodData) {
+      console.log(foodData);
+
+      for (var i = 0; i < 4; i++) {
+        $(`#search-result-${i}`).empty();
+        $(`#search-result-${i}`).append(`
+                <h2>Heading</h2>
+                <div>data</div>
+            `);
+      }
     });
-    
-};
+  });
+}
 
-// Search button handler
-$("#search-btn").on("click", foodSearch);
+function foodSearch(evt) {
+  evt.preventDefault();
 
+  var searchInput = $("#food-search-input").val().trim();
 
-function exerciseSearch(){
+  // quit function if no input when button clicked
+  if (searchInput === null) {
+    return;
+  }
 
-    /* options for searches replaces exercises
+  // remove search from bar
+  $("#search-input").val("");
+
+  foodSearchResults(searchInput);
+}
+
+// Food search button handler
+$("#food-search-btn").on("click", foodSearch);
+
+/* options for searches replaces exercises
     bodypart - waist, upper legs, back, lower legs, chest, upper arms, cardio, shoulders, lower arms, 
     equipment - body weight, cable, leverage machine, assisted, medicine ball, stability ball, 
                 band, barbell, dumbbell, kettlebell, sled machine, smith machine, hammer, rope,
                 tire, trap bar, stationary bike, wheel roller, weighted, roller, ez barbell
 
     */
-fetch("https://exercisedb.p.rapidapi.com/exercises", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "exercisedb.p.rapidapi.com",
-		"x-rapidapi-key": "947315c5d7msh8e150cb96f6debap19e15cjsn514f7b11e3e3"
-	}
-})
-.then(response => {
-	console.log(response);
-    response.json().then(function(data){
-        console.log(data);
+function exerciseSearch() {
+  fetch("https://exercisedb.p.rapidapi.com/exercises", {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+      "x-rapidapi-key": "947315c5d7msh8e150cb96f6debap19e15cjsn514f7b11e3e3",
+    },
+  })
+    .then((response) => {
+      console.log(response);
+      if (!response.ok) {
+        throw Error("ERROR");
+      }
+      return response.json();
     })
-})
-.catch(err => {
-	console.error(err);
-});
-};
+    .then((data) => {
+      console.log(data);
+      var exerciseData = [data];
+      var bodyPart = []
+             
+      console.log(exerciseData[0][0]);
+    })
+    // .catch((err) => {
+    //   console.error(err);
+    // });
+}
 
 exerciseSearch();
-
 
